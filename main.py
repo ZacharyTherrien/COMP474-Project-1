453e45f3c4208299eb582e184366d515f83415e6
Zachary Therrien <ZTherrien.work@gmail.com>
2025-03-09 18:58:05 -0400

    Implemented NLP
    Implemented basic NLP for the chatbot. Using keywords, the chatbot will give a response given the first keyword found in the user's input.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 #Define responses with dictionary, targeted for students learning OOP
-reponses = {
+responses = {
     "default" : "I'm sorry, I didn't understand that. Could you rephrase?",
     "greet" : "Hello! can I help you?",
     "bye" : "Goodbye! Have a great day!",
@@ -13,20 +13,40 @@ reponses = {
     "loop" : "A loop is used for..."
 }
 
+keywords = {
+    "greet" : {"hi", "hello", "greetings", "how are you"},
+    "bye" : {"bye", "goodbye", "see you", "have a good day"}
+}
+
 #Process User Input with NLP
-def get_response(response):
-    return ""
+def get_response(input):
+    #Process user input with NLP model
+    statement = nlp(input)
+    response = responses["greet"]
+    for token in statement:
+        k = token.text.lower()
+        for key, value in keywords.items():
+            if k in value:
+                response = responses[key]
+                return response
+    
+    # for token in statement:
+    #     key = token.text.lower()
+    #     if key in responses:
+    #         response = responses[key]
+    #     else:
+    #         response = responses["default"]
+    return response
 
 #User Interaction Loop
 def chat():
     print("Chatbot: Hello! Enter your question for help or 'bye' to exit.")
     while True:
         user_input = input("You: ")
-        if user_input.lower() == "bye":
-            break
         response = get_response(user_input)
         print("Chatbot: ", response)
-    print("Chatbot: Goodbye!")
+        if response == responses["bye"]:
+            break
 
 if __name__ == "__main__":
     chat()
